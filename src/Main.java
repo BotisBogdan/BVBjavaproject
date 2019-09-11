3348ff61d3f10e47fbd031d9ee9c30821f8cbed5
Duna Paul <p24664@uniper.energy>
2019-09-11 20:52:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Heloo world");
+
+        // Tipuri de date
+        String tara = "Spania";
+        String oras = "Barcelona";
+        byte districts = 10;
+        float Area = 101.4f;
+
+
+
+
+
+
+
+
+    }
+}
